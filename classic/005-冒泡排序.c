bb09e89edd90a7ec99f245383ad972df897793f6
siyue777 <31797294+siyue777@users.noreply.github.com>
2018-10-30 22:17:22 +0800

    Update 005-冒泡排序.c

--- classic/005-冒泡排序.c
+++ classic/005-冒泡排序.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 int main(void)
 {
-	int n;    //输入n个数
-	int a;
-	int i,j;
-	int max;
-	scanf("%d",&n);
-	char num[n];
-	for(a=0;a<n;a++){    		
-  scanf("%d",&num[a]);    
+    int n;    //输入n个数
+    int a;
+    int i,j;
+    int max;
+    scanf("%d",&n);
+    char num[n];
+    for(a=0;a<n;a++){
+        scanf("%d",&num[a]);    
+    }
+    for(i=0;i<n-1;i++){          //n个数需要比对n-1次
+        for(j=i+1;j<n;j++){      
+            if(num[i]<num[j]){     //如果后一项比前一项大，则交换位置
+                max=num[j];
+                num[j]=num[i];
+		num[i]=max;
+	    }
 	}
-	for(i=0;i<n-1;i++){          //n个数需要比对n-1次
-		for(j=i+1;j<n;j++){      
-			if(num[i]<num[j]){     //如果后一项比前一项大，则交换位置
-				max=num[j];
-				num[j]=num[i];
-				num[i]=max;
-			}
-		}
-	}
-	for(a=0;a<n;a++){
-		printf("%d",num[a]);
-	}
-	return 0;
+    }
+    for(a=0;a<n;a++){
+        printf("%d",num[a]);
+    }
+    return 0;
 }
