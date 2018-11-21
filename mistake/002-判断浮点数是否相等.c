1d2d5cf896f03e6e926bd9900b83f7c09924123b
siyue777 <31797294+siyue777@users.noreply.github.com>
2018-11-21 21:23:48 +0800

    Update 002-判断浮点数是否相等.c

--- mistake/002-判断浮点数是否相等.c
+++ mistake/002-判断浮点数是否相等.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 int main()
 {
-    double a,b;
-    scanf("%lf %lf",&a,&b);
-    if((a-b)<=1e-8&&(a-b)>=-1e-8) //一开始只写了（a-b）<=1e-8,这种在a大于b的情况下成立还有a<b的情况，问题考虑得太简单
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    if((a - b) <= 1e - 8 && (a - b) >= -1e - 8) //一开始只写了（a-b）<=1e-8,这种在a大于b的情况下成立还有a<b的情况，问题考虑得太简单
         printf("yes\n");
     else
         printf("no\n");
