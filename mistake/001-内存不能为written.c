52248ac02e7ae29402faecf628e4e5f30ceb7d50
siyue777 <31797294+siyue777@users.noreply.github.com>
2018-11-21 21:22:59 +0800

    Update 001-内存不能为written.c

--- mistake/001-内存不能为written.c
+++ mistake/001-内存不能为written.c
@@ -3,21 +3,21 @@
 int main(void)
 {
     int n;
-    scanf("%d",&n);
-    int a,m[n];
+    scanf("%d", &n);
+    int a, m[n];
     
-    for(a=0;a<n;a++)
-    	scanf("%d",&m[a]); //！报错原因：忘记加上地址运算符"&"
+    for (a = 0; a < n; a++)
+    	scanf("%d", &m[a]); //！报错原因：忘记加上地址运算符"&"
     	
-    int min=m[0];
-    int max=m[0];
-    for(a=1;a<n;a++)
+    int min = m[0];
+    int max = m[0];
+    for (a = 1; a < n; a++)
     {
-    	if(m[a]<min)
-    		min=m[a];
-    	if(m[a]>max)
-    		max=m[a];
+    	if(m[a] < min)
+	    min=m[a];
+    	if(m[a] > max)
+    	    max=m[a];
 	}
-	printf("%d %d",min,max);
+	printf("%d %d", min, max);
     return 0;
 }
